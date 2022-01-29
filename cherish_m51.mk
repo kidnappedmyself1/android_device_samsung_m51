5e67e0f0a0ee193f06644e60e9acf8b4c89cdbba
kidnappedmyself1 <97450347+kidnappedmyself1@users.noreply.github.com>
2022-01-29 13:06:23 +0530

    Update and rename lineage_m51.mk to cherish_m51.mk

--- cherish_m51.mk
+++ cherish_m51.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/m51/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_m51
+PRODUCT_NAME := cherish_m51
 PRODUCT_DEVICE := m51
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-M515F
