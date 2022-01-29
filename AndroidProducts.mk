821ac1511afd0c3881979ec2755f07995b4b54b1
kidnappedmyself1 <97450347+kidnappedmyself1@users.noreply.github.com>
2022-01-29 13:04:47 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_m51.mk
+    $(LOCAL_DIR)/cherish_m51.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_m51-user \
-    lineage_m51-userdebug \
-    lineage_m51-eng
+    cherish_m51-user \
+    cherish_m51-userdebug \
+    cherish_m51-eng
