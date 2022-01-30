87511f67993368259d9090b406eef089bc04827c
kidnappedmyself1 <97450347+kidnappedmyself1@users.noreply.github.com>
2022-01-30 20:24:19 +0530

    Update arrow_m51.mk

--- arrow_m51.mk
+++ arrow_m51.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/m51/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/arrow/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := arrow_m51
